df11f7a5391921579a180ff138f480a56a35bbc1
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-11-29 16:27:51 +0400

    add skeleton for loading

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,15 +3,19 @@ import Hero from "../Hero/Hero";
 import { useEffect, useState } from "react";
 import { getNews } from "../../API/apiNews";
 import NewsList from "../NewsList/NewsList";
+import Skeleton from "../Skeleton/Skeleton";
 
 const Main = () => {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true);
         const response = await getNews();
         console.log(response);
+        setIsLoading(false);
         setNews(response.articles);
       } catch (err) {
         console.log(err);
@@ -22,8 +26,16 @@ const Main = () => {
 
   return (
     <div className={styles.main}>
-      {news.length > 0 && <Hero item={news[0]} />}
-      {news.length > 0 && <NewsList news={news} />}
+      {news.length > 0 && !isLoading ? (
+        <Hero item={news[0]} />
+      ) : (
+        <Skeleton count={1} type="banner" />
+      )}
+      {news.length > 0 && !isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Skeleton count={5} type="item" />
+      )}
     </div>
   );
 };
